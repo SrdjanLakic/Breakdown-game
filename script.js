a8e6f2f7fad7285a1f6bae533bda97a0fbc5aa3c
SrdjanLakic <srdjan949@gmail.com>
2022-05-31 18:43:18 -0400

    class Block added

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ const grid = document.querySelector(".grid");
 const blockWidth = 100;
 const blockHeight = 20;
 
+class Block {
+  constructor(xAxis, yAxis) {
+    this.bottomLeft = [xAxis, yAxis];
+    this.bottomRight = [xAxis + blockWidth, yAxis];
+    this.topLeft = [xAxis, yAxis + blockHeight];
+    this.topRight = [xAxis + blockWidth, yAxis + blockHeight];
+  }
+}
+
 function addBlock() {
   const block = document.createElement("div");
   block.classList.add("block");
